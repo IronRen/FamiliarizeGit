7b11d4d5688e1afc08ecc1818f0bda8a924a4176
AdeRen <2970664313@qq.com>
2019-05-14 11:24:53 +0800

    xiugai3

--- TestOne.java
+++ TestOne.java
@@ -1,9 +1,5 @@
 public class TestOne{
  System.out.println("TestOne...");
  System.out.println("TestTwo...");
- System.out.println("TestThree...");
-<<<<<<< Updated upstream
  System.out.println("TestFour...");
-=======
->>>>>>> Stashed changes
 }

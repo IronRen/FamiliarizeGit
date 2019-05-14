963803b895604908c27e895e4024889140a4f94e
AdeRen <2970664313@qq.com>
2019-05-14 11:22:42 +0800

    修改2

--- TestOne.java
+++ TestOne.java
@@ -2,5 +2,8 @@ public class TestOne{
  System.out.println("TestOne...");
  System.out.println("TestTwo...");
  System.out.println("TestThree...");
+<<<<<<< Updated upstream
  System.out.println("TestFour...");
+=======
+>>>>>>> Stashed changes
 }
